b727bba624bd9bb2d062ae487c43820264733961
JoseOgam <jotienoochieng@gmail.com>
2021-01-27 14:44:56 -0500

    modified redux files

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { createStore } from 'redux'
+import { devToolsEnhancer } from 'redux-devtools-extension'
 import tasks from './reducers'
-const store = createStore(tasks)
+const store = createStore(tasks, devToolsEnhancer())
 
 export default store
