c9edbcef6bc1e3f123aafcfad44c148a2f619f0a
JoseOgam <jotienoochieng@gmail.com>
2021-01-28 12:07:28 -0500

    apply middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux'
-import { devToolsEnhancer } from 'redux-devtools-extension'
+import { applyMiddleware, createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
 import tasks from './reducers'
-const store = createStore(tasks, devToolsEnhancer())
+const store = createStore(tasks, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store
